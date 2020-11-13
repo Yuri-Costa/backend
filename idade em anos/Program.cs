ecf18dda452119e49b78fb7c21985720c7866b7a
DEV <dev@info.local>
2020-11-13 16:49:34 -0300

    calculdora idade

---
+++ idade em anos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace idade_em_anos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("digite sua idade");
+        int idade = int.Parse(Console.ReadLine());
+
+        int meses = idade * 12;
+        int dias = idade * 365;
+        int horas = dias* 24;
+        int minutos = horas* 60;
+
+        Console.WriteLine("Voce viveu");
+        Console.WriteLine( meses+"meses");
+        Console.WriteLine( dias+"dias");
+        Console.WriteLine(horas+"horas");
+        Console.WriteLine(minutos+"minutos");
+            
+
+            
+        }
+    }
+}
